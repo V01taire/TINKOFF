a1b5114c4f96436488803cc1f830610f35a029be
V01taire <egormikhaylick@yandex.ru>
2023-10-17 23:04:49 +0300

    Готовое ДЗ 1 (переделано)

--- src/main/java/edu/hw1/Task3.java
+++ src/main/java/edu/hw1/Task3.java
@@ -1,35 +1,21 @@
 package edu.hw1;
 
+import java.util.Arrays;
+
 @SuppressWarnings("HideUtilityClassConstructor")
 public class Task3 {
 
     public static boolean isNestable(int[] array1, int[] array2) {
-
         if (array1.length == 0 || array2.length == 0) {
             return false;
         }
 
-        int minArray1 = findMin(array1);
-        int maxArray1 = findMax(array1);
-        int minArray2 = findMin(array2);
-        int maxArray2 = findMax(array2);
+        int minArray1 = Arrays.stream(array1).min().orElse(Integer.MAX_VALUE);
+        int maxArray1 = Arrays.stream(array1).max().orElse(Integer.MIN_VALUE);
+        int minArray2 = Arrays.stream(array2).min().orElse(Integer.MAX_VALUE);
+        int maxArray2 = Arrays.stream(array2).max().orElse(Integer.MIN_VALUE);
 
         return minArray1 > minArray2 && maxArray1 < maxArray2;
     }
-
-    private static int findMin(int[] array) {
-        int min = array[0];
-        for (int num : array) {
-            min = Math.min(min, num);
-        }
-        return min;
-    }
-
-    private static int findMax(int[] array) {
-        int max = array[0];
-        for (int num : array) {
-            max = Math.max(max, num);
-        }
-        return max;
-    }
 }
+
