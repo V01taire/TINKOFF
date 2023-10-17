dc21d52e33b2d74a7f0c7acdb25ba8b7fcada082
V01taire <egormikhaylick@yandex.ru>
2023-10-17 23:50:12 +0300

    Готовое ДЗ 1 (переделано)

--- src/test/java/edu/hw1/Task3Test.java
+++ src/test/java/edu/hw1/Task3Test.java
@@ -1,41 +1,36 @@
 package edu.hw1;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-import java.util.Arrays;
-import java.util.Collection;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(Parameterized.class)
 public class Task3Test {
-    private int[] array1;
-    private int[] array2;
-    private boolean expected;
 
-    public Task3Test(int[] array1, int[] array2, boolean expected) {
-        this.array1 = array1;
-        this.array2 = array2;
-        this.expected = expected;
+    @ParameterizedTest
+    @MethodSource("provideInputParameter")
+    void isNestableTest(int[] firstArray, int[] secondArray, boolean expected) {
+
+        boolean result = Task3.isNestable(firstArray, secondArray);
+
+        assertThat(result)
+                .isEqualTo(expected);
     }
 
-    @Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-            {new int[]{1, 2, 3, 4}, new int[]{0, 6}, true},
-            {new int[]{3, 1}, new int[]{4, 0}, true},
-            {new int[]{9, 9, 8}, new int[]{8, 9}, false},
-            {new int[]{1, 2, 3, 4}, new int[]{2, 3}, false},
-            {new int[]{}, new int[]{1, 2, 3}, false},
-            {new int[]{1, 2, 3}, new int[]{}, false},
-            {new int[]{}, new int[]{}, false}
-        });
-    }
-
-    @Test
-    public void isNestableTest() {
-        assertEquals(expected, Task3.isNestable(array1, array2));
+    private static Stream<Arguments> provideInputParameter() {
+        return Stream.of(
+                Arguments.of(new int[] {1, 2, 3, 4}, new int[] {0, 6}, true),
+                Arguments.of(new int[] {3, 1}, new int[] {4, 0}, true),
+                Arguments.of(new int[] {9, 9, 8}, new int[] {8, 9}, false),
+                Arguments.of(new int[] {1, 2, 3, 4}, new int[] {2, 3}, false),
+                Arguments.of(new int[] {1, 2, 3}, new int[] {}, false),
+                Arguments.of(new int[] {}, new int[] {1, 2, 3}, false),
+                Arguments.of(new int[] {1}, new int[] {0, 3}, true),
+                Arguments.of(new int[] {}, new int[] {2, 3}, false),
+                Arguments.of(new int[] {1, 3}, new int[] {2}, false),
+                Arguments.of(new int[] {}, new int[] {}, false)
+        );
     }
 }
 
